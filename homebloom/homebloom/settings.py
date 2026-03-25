babb4d4c16c6c4d019c1a7b370f0bca2f76ea9ce
Gopika <gopikak75317@gmail.com>
2026-03-25 13:32:48 +0530

    fix server crash

--- homebloom/homebloom/settings.py
+++ homebloom/homebloom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ["housebloom.shop", "www.housebloom.shop", ".onrender.com"]
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://housebloom.shop",
