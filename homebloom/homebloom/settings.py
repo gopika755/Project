780a3e679a9286b5345a4b9f9ee9c7a5688c3412
Gopika <gopikak75317@gmail.com>
2026-05-12 22:36:09 +0530

    fixed neon ssl connection

--- homebloom/homebloom/settings.py
+++ homebloom/homebloom/settings.py
@@ -96,8 +96,12 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': config('DB_SSLMODE', default='require'),
+        },
     }
 }
+
 
 
 # Password validation
