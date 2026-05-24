e02d4d7c1bdae78a775595b2660df4c0348932f0
Gopika <gopikak75317@gmail.com>
2026-05-24 20:26:00 +0530

    new changes

--- homebloom/homebloom/settings.py
+++ homebloom/homebloom/settings.py
@@ -87,13 +87,13 @@ WSGI_APPLICATION = 'homebloom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
