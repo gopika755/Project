4213e3bcb80d15ac01f4ad94e8d74f0016b767e8
Gopika <gopikak75317@gmail.com>
2026-06-16 15:14:58 +0530

    small change

--- homebloom/homebloom/settings.py
+++ homebloom/homebloom/settings.py
@@ -176,8 +176,7 @@ AUTH_USER_MODEL = 'bloom.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
